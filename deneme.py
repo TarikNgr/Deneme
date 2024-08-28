0725879e711e06cdc83c2d0f33e40a69cbda66a3
Tarık Nigar <152701450+TarikNgr@users.noreply.github.com>
2024-08-28 11:58:17 +0300

    TASK-11 Denemeee

--- deneme.py
+++ deneme.py
@@ -1,7 +1,7 @@
 import tkinter as tk #provides a library of basic elements of GUI widgets
 from tkinter import messagebox #provides a different set of dialogues that are used to display message boxes
 import random
-   
+    
 def check_winner(board, player):
     # Check rows, columns, and diagonals for a win
     for i in range(3):
